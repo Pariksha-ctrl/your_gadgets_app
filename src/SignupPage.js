095717d6b1c89af3d15fdd13399cdb6c7d93751f
Pariksha Roka <parikshanepali@gmail.com>
2021-04-22 00:11:46 +1000

    UI of Sign up page is done.

--- src/SignupPage.js
+++ src/SignupPage.js
@@ -1,12 +1,49 @@
 import * as React from 'react';
-import {Button, Text, View} from 'react-native';
+import {
+  Button,
+  Text,
+  View,
+  TextInput,
+  StyleSheet,
+  SafeAreaView,
+} from 'react-native';
 
 export default function Signup({navigation}) {
+  function displaySignupForm() {
+    return (
+      <SafeAreaView>
+        <Text>Username:</Text>
+        <TextInput style={styles.input} />
+
+        <Text>Password:</Text>
+        <TextInput style={styles.input} secureTextEntry={true} />
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      {/* We will navigate to login page using navigation */}
-      <Text>Welcome New User to Gadgets App!</Text>
-      <Button title="signup" onPress={() => navigation.navigate('LoginPage')} />
-    </View>
+    <>
+      {displaySignupForm()}
+      <View>
+        <Button
+          style={styles.signupButton}
+          title="signup"
+          onPress={() => navigation.navigate('LoginPage')}
+        />
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+  },
+  signupButton: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
