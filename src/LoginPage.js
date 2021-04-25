1a7bb7ef9d62d95e78d768d5ccc5429ec7b959ea
Pariksha Roka <parikshanepali@gmail.com>
2021-04-25 10:55:28 +1000

    connected backend service to get the new user values and save it.

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -2,6 +2,27 @@ import * as React from 'react';
 import {Button, View} from 'react-native';
 
 export default function Login({navigation}) {
+  function displayLoginForm() {
+    return (
+      <SafeAreaView>
+        <Text>Username:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setUsername}
+          value={username}
+        />
+
+        <Text>Password:</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          onChangeText={setPassword}
+          value={password}
+        />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       {/* We will navigate to home page using navigation */}
--- src/SignupPage.js
+++ src/SignupPage.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import {
   Button,
   Text,
@@ -6,19 +6,56 @@ import {
   TextInput,
   StyleSheet,
   SafeAreaView,
+  Alert,
 } from 'react-native';
 
 export default function Signup({navigation}) {
+  // capturing user data
+  // declaring the state for username and password
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   function displaySignupForm() {
     return (
       <SafeAreaView>
         <Text>Username:</Text>
-        <TextInput style={styles.input} />
+        <TextInput
+          style={styles.input}
+          onChangeText={setUsername}
+          value={username}
+        />
 
         <Text>Password:</Text>
-        <TextInput style={styles.input} secureTextEntry={true} />
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          onChangeText={setPassword}
+          value={password}
+        />
       </SafeAreaView>
     );
+  }
+
+  async function callCreatedUser() {
+    const reply = await fetch('http://192.168.1.105:3000/create', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json', // application content will be sent to JSON
+      },
+      // the login ceredentials given by the user will be sent to the service
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+    // navigating newly created user to the login page
+    if (reply.status == 200) {
+      navigation.navigate('LoginPage');
+    } else {
+      Alert.alert('Error', 'New user cannot be created!');
+      console.log('Error');
+    }
   }
 
   return (
@@ -28,7 +65,10 @@ export default function Signup({navigation}) {
         <Button
           style={styles.signupButton}
           title="signup"
-          onPress={() => navigation.navigate('LoginPage')}
+          onPress={() => {
+            // calling the serviceh
+            callCreatedUser();
+          }}
         />
       </View>
     </>
